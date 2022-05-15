08b16347c757ec3c8503a6541eada23bd9008eda
teyfursaidov <teyfursaidov1991@gmail.com>
2022-05-15 21:19:32 +0300

    Solar System Basic Structure Ready.

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,19 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Branching scenario
+        /*
+        * Create a basic structure of solar system
+        *  and deploy it as soon as possible.*/
+
+
+        SolarSystem factorX = new SolarSystem();
+        factorX.sun = "1";
+        factorX.moon = "4";
+        factorX.planet = "2";
+        factorX.stars = "10000";
+
+    }
+}
---
+++ src/com/syntax/SolarSystem.java
@@ -0,0 +1,11 @@
+package com.syntax;
+
+public class SolarSystem {
+
+    String sun;
+    String moon;
+    String planet;
+    String stars;
+
+
+}

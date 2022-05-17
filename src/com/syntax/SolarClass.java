b072b07e4cd37f6a3e76eecdb269326c16f655a9
teyfursaidov <teyfursaidov1991@gmail.com>
2022-05-18 02:18:36 +0300

    Solar System New Update Code.

---
+++ src/com/syntax/SolarClass.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class SolarClass {
+    public static void main(String[] args) {
+
+
+        SolarSystem solarFactor = new SolarSystem();
+        solarFactor.sun = "1";
+        solarFactor.moon = "4";
+        solarFactor.planet = "2";
+        solarFactor.stars = "10000";
+
+    }
+}
